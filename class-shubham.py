7a5fa7c22a2fc947b73831c0d16758ea002279da
Shubham Dubey <Shubham.Dubey@siemens.com>
2023-03-26 15:33:29 +0530

    first

---
+++ class-shubham.py
@@ -0,0 +1,19 @@
+class Shubham:
+    def __init__(self,name,age) -> None:
+        self.name =name
+        self.age = age
+
+
+    def name_age(self):
+        print(f'My name is {self.name} and my age is {self.age}')
+
+
+
+
+
+
+hero=Shubham("Shubham",28)
+
+
+hero.name_age()
+        
---
+++ first-class.py
@@ -0,0 +1,15 @@
+class Shubham:
+    def __init__(self,name,dog) :
+           self.name = name
+           self.dog = dog
+
+
+    def __str__(self) :
+          
+            return(self.dog)
+        
+
+p1= Shubham("shubham","whiskey")
+
+print(p1)
+
